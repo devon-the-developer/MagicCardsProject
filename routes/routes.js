b2c372359e90d5a0567902d1a658e8b7af889bee
devonL <devonlumsden@mail.com>
2019-03-30 22:36:26 +1300

    add view of single card

--- routes/routes.js
+++ routes/routes.js
@@ -7,6 +7,12 @@ ROUTER.get('/', function (req, res) {
   res.render("../views/cards/index", CARDSDATA)
 })
 
+ROUTER.get('/cards/:name', function (req, res) {
+  let cardName = req.params.name;
+  let grabbedCard = CARDSDATA.cards.find(x => x.name == cardName);
+  res.render('../views/cards/view', grabbedCard)
+} )
+
 
 
 module.exports = ROUTER
